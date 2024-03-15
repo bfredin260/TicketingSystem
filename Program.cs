33d8cbf16824f881c346ffa31e3a68357f54e6bd
Braedon Fredin <b260fredin@gmail.com>
2024-03-15 17:32:29 -0500

    [2.01] Added methods to get user input in main.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,61 @@
 ﻿using NLog;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
-string file = "Tickets.csv";
-string userChoice;
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+
+string? choice;
 
 List<Ticket> tickets = new List<Ticket>();
 
-Console.WriteLine("\n");
+Console.WriteLine("\n   TICKETING SYSTEM\n----------------------");
 
 do{
-    Console.WriteLine("Select an Option:\n\n1) Add Ticket(s)\n2) View Tickets");
+    choice = getChoice();
 
-} while(true);
+} while(choice != "");
+
+
+string? getInput(string prompt) {
+    Console.Write(prompt);
+    
+    return Console.ReadLine();
+}
+
+string? getChoice() {
+    string? inp = getInput("\n\n SELECT:\n---------\n\n1) Add Ticket(s)\n2) View Tickets\n\nEnter to exit\n\n> ");
+    string? ch = "";
+    
+    do {
+        if(inp != "") {
+            ch = inp.ToLower()
+                        .ToCharArray()[0]
+                        .ToString()
+            ;
+
+            Console.WriteLine();
+
+            if(ch == "1" || ch == "2") {
+                logger.Info($"Input: \"{ch}\"\n");
+
+                string sel;
+                switch(ch) {
+                    case "1":
+                        sel = "Add Ticket(s)";
+                        break;
+                    default:
+                        sel = "View Tickets";
+                        break;
+                }
+
+                logger.Info($"Selected: \"{sel}\"");
+            } else {
+                logger.Error("Please enter a valid option.");
+            }
+        }
+    } while (ch != "1" && ch != "2" && ch != "");
+
+    return ch;
+}
